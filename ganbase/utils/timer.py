36705a025793a84da52cdc7ae29c2153f7479dbe
lizz <363664470@qq.com>
2018-03-16 21:09:45 +0800

    timer good print

--- ganbase/utils/timer.py
+++ ganbase/utils/timer.py
@@ -1,10 +1,62 @@
 from time import time
+from string import Formatter
+from datetime import timedelta
 
 
 class TimerError(Exception):
     def __init__(self, message):
         self.message = message
         super(TimerError, self).__init__(message)
+
+
+def strfdelta(tdelta,
+              fmt='{D:02}d {H:02}h {M:02}m {S:02}s',
+              inputtype='timedelta'):
+    """Convert a datetime.timedelta object or a regular number to a custom-
+    formatted string, just like the stftime() method does for datetime.datetime
+    objects.
+
+    The fmt argument allows custom formatting to be specified.  Fields can
+    include seconds, minutes, hours, days, and weeks.  Each field is optional.
+
+    Some examples:
+        '{D:02}d {H:02}h {M:02}m {S:02}s' --> '05d 08h 04m 02s' (default)
+        '{W}w {D}d {H}:{M:02}:{S:02}'     --> '4w 5d 8:04:02'
+        '{D:2}d {H:2}:{M:02}:{S:02}'      --> ' 5d  8:04:02'
+        '{H}h {S}s'                       --> '72h 800s'
+
+    The inputtype argument allows tdelta to be a regular number instead of the
+    default, which is a datetime.timedelta object.  Valid inputtype strings:
+        's', 'seconds',
+        'm', 'minutes',
+        'h', 'hours',
+        'd', 'days',
+        'w', 'weeks'
+    """
+
+    # Convert tdelta to integer seconds.
+    if inputtype == 'timedelta':
+        remainder = int(tdelta.total_seconds())
+    elif inputtype in ['s', 'seconds']:
+        remainder = int(tdelta)
+    elif inputtype in ['m', 'minutes']:
+        remainder = int(tdelta) * 60
+    elif inputtype in ['h', 'hours']:
+        remainder = int(tdelta) * 3600
+    elif inputtype in ['d', 'days']:
+        remainder = int(tdelta) * 86400
+    elif inputtype in ['w', 'weeks']:
+        remainder = int(tdelta) * 604800
+
+    f = Formatter()
+    desired_fields = [field_tuple[1] for field_tuple in f.parse(fmt)]
+    possible_fields = ('W', 'D', 'H', 'M', 'S')
+    constants = {'W': 604800, 'D': 86400, 'H': 3600, 'M': 60, 'S': 1}
+    values = {}
+    for field in possible_fields:
+        if field in desired_fields and field in constants:
+            values[field], remainder = divmod(remainder, constants[field])
+    return f.format(fmt, **values)
 
 
 class Timer(object):
@@ -84,6 +136,14 @@ class Timer(object):
         dur = time() - self._t_last
         self._t_last = time()
         return dur
+
+    def now(self, fmt='{D:01}d {H:02}:{M:02}:{S:02}'):
+        """Time from start"""
+        if not self._is_running:
+            raise TimerError('timer is not running')
+        self._t_last = time()
+        delta = timedelta(seconds=self._t_last - self._t_start)
+        return strfdelta(delta, fmt=fmt)
 
 
 _g_timers = {}  # global timers
