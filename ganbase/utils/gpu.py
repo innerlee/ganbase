f9515e4c505d0bcaa1efd63ae1430756ca845b56
lizz <lizz@lab-112.plab.ie.cuhk.edu.hk>
2018-04-03 22:48:29 +0800

    occupy_gpu

--- ganbase/utils/gpu.py
+++ ganbase/utils/gpu.py
@@ -1,5 +1,16 @@
 import os
+import torch
 
 
 def visible_gpu(gpus):
+    """
+        set visible gpu.
+    """
     os.environ['CUDA_VISIBLE_DEVICES'] = str(gpus)
+
+
+def occupy_gpu():
+    """
+        make program appear on nvidia-smi.
+    """
+    torch.zeros(1).cuda()
